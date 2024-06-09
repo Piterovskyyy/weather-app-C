116f8f68ff39cc25ed332fb7eeb158561bb88e57
Piter <jakub.piotrowski666@gmail.com>
2024-06-09 13:54:05 +0200

    add qt network

--- SourceFiles/main.cpp
+++ SourceFiles/main.cpp
@@ -1,6 +1,10 @@
 #include "../headerFiles/mainwindow.h"
-
 #include <QApplication>
+#include <QDebug>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/QNetworkReply>
+
 
 int main(int argc, char *argv[])
 {
@@ -8,5 +12,30 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.setFixedSize(1000,667);
     w.show();
+
+    // Tworzymy menedżera dostępu do sieci
+    QNetworkAccessManager manager;
+
+    // Tworzymy żądanie do wykonania
+    QNetworkRequest request(QUrl("http://example.com"));
+
+    // Wykonujemy żądanie i otrzymujemy odpowiedź
+    QNetworkReply *reply = manager.get(request);
+
+    // Obsługujemy odpowiedź
+    QObject::connect(reply, &QNetworkReply::finished, [&]() {
+        if (reply->error() == QNetworkReply::NoError) {
+            // Odczytujemy odpowiedź
+            QByteArray responseData = reply->readAll();
+            qDebug() << "Response:" << responseData;
+        } else {
+            // Obsługujemy błąd odpowiedzi
+            qDebug() << "Error:" << reply->errorString();
+        }
+
+        // Usuwamy obiekt odpowiedzi
+        reply->deleteLater();
+    });
+
     return a.exec();
 }
